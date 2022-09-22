5ef8fde1d6618bcaaf518c7ba6fd751ee87c1b27
CarmenFF-pixel <99807019+CarmenFF-pixel@users.noreply.github.com>
2022-09-22 21:49:22 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -48,9 +48,4 @@ window.onload = function() {
             new TxtType(elements[i], JSON.parse(toRotate), period);
         }
     }
-    // INJECT CSS
-    var css = document.createElement("style");
-    css.type = "text/css";
-    css.innerHTML = ".typewrite > .wrap { border-right: 0.13em solid #f29f05}";
-    document.body.appendChild(css);
 };
